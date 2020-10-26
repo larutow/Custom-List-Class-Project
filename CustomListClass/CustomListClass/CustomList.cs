aebb3acb1120572c8de30adc0c3a58c4939ca769
larutow <lucas.rutowski@gmail.com>
2020-10-26 14:03:31 -0500

    added count & capacity public properties, member variables, custom list constructor

--- CustomListClass/CustomListClass/CustomList.cs
+++ CustomListClass/CustomListClass/CustomList.cs
@@ -8,5 +8,26 @@ namespace CustomListClass
 {
     public class CustomList<T>
     {
+        private T[] _array;
+        private int _count;
+        private int _capacity;
+        public int Count{
+            get
+            {
+                return _count;
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return _capacity;
+            }
+        }
+        public CustomList()
+        {
+            //initializes list w/ default capacity (4)
+            _array = new T[4];
+        }
     }
 }
